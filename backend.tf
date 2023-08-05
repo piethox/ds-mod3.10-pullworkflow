b3c16c33068be228b861cc5a5d1d6a40db9ed8b2
piethox <dsuntama@gmail.com>
2023-08-05 11:01:14 +0800

    del backend and provider

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce2-tfstate-bkt"
-    key    = "david-s3-workflows.tfstate"   #Change the value  of this to yourname-ecs-1.tfstate for  example
-    region = "ap-southeast-1"
-  }
-}
