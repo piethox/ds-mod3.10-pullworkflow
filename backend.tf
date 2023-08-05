04b769764f83509794002f6ff622a40071dcc33f
piethox <dsuntama@gmail.com>
2023-08-05 10:54:11 +0800

    edit backend from Dev

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce2-tfstate-bkt"
-    key    = "david-s3-workflow.tfstate"   #Change the value  of this to yourname-ecs-1.tfstate for  example
+    key    = "david-s3-workflows.tfstate"   #Change the value  of this to yourname-ecs-1.tfstate for  example
     region = "ap-southeast-1"
   }
 }
